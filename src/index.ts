f3d4305326f3170b2395cc3d7e2b83e16c3a20a8
Cade Richard <cade@caderichard.com>
2021-05-02 13:26:33 -0400

    Heroku test w/express

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
-const hello = () => {
-  const i = "world";
-};
+import express from "express";
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.get("/", (_, res) => res.send("Hello World!"));
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
