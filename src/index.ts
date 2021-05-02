61d36cd1e7e72c98f4787d3016e2e98ff58b2b29
Cade Richard <cade@caderichard.com>
2021-05-02 16:36:50 -0400

    Add express back to prevent crashing :P

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import ButchBot from "./butchbot";
+import express from "express";
 
 if (!process.env.BOT_TOKEN) {
   throw Error("No BOT_TOKEN provided! Check your env vars.");
@@ -6,11 +7,7 @@ if (!process.env.BOT_TOKEN) {
 
 ButchBot(process.env.BOT_TOKEN);
 
-/*
-import express from "express";
-
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.get("/", (_, res) => res.send("Hello World!"));
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
-*/
